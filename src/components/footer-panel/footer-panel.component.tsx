dae7d89c7645d8d811d85e7b440a55a244ec36b0
Morganti, Vittorio || mytheresa.com <Vittorio.Morganti@mytheresa.com>
2020-03-08 17:06:12 +0100

    Refactor

--- src/components/footer-panel/footer-panel.component.tsx
+++ src/components/footer-panel/footer-panel.component.tsx
@@ -2,23 +2,20 @@ import React from "react"
 import { Button } from "../button/button.component"
 import { MainSketch } from "../../sketches/sketch"
 import { Container } from "./footer-panel.style"
-import {TimeForm} from "../control-panel/time-form/time-form.component"
-
+import { TimeForm } from "../control-panel/time-form/time-form.component"
 
 export class FooterPanel extends React.Component<any, any> {
-    public render () {
-        return(
+    public render() {
+        return (
             <Container>
-        <div className="alert alert-info" role="alert">
-            <strong>Polyrhythm created: </strong> 
-        </div>
-        
-        
-        <div className="alert alert-info" role="alert">
-            <strong>Polymeter created:</strong>
-        </div>
-        </Container>
+                <div className="alert alert-info" role="alert">
+                    <strong>Polyrhythm created: </strong>
+                </div>
+
+                <div className="alert alert-info" role="alert">
+                    <strong>Polymeter created:</strong>
+                </div>
+            </Container>
         )
     }
-        
 }
