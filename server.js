896062bc01d06a7fb8dc3129e28a9414b9d7f725
kaikura <antonioic94@gmail.com>
2020-03-10 17:37:23 +0100

    ri-commit with unmodified server.js

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const rotaryEncoder = require('./rotatoryE');
 
 const express = require('express');
+// changed 'io' with 'socket'
+const socket = require('socket.io');
+
 const bodyParser = require('body-parser')
 const path = require('path');
+//APP setup
 const app = express();
-// PER POT
-//const { Board, Sensor } = require("johnny-five");
-
+var server = app.listen(8080, function(){
+  console.log("listening to request on port 8080")
+});
 const five = require("johnny-five");
 const board = new five.Board();
 
-const io = require('socket.io')();
+// Socket setup
+var io = socket(server);
 
+io.on('connection', function(socket){
+  console.log('made socket connection', socket.id)
+})
+io.listen(8000);
+
+//Static files
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/ping', function (req, res) {
@@ -23,45 +34,56 @@ app.get('/', function (req, res) {
 });
 
 
+val=0;
 
+var enc_val = '';
 
+board.on('ready', () => {
 
- var enc_val = '';
-  board.on('ready', () => {
-    const upButton = new five.Button(13);
-    const downButton = new five.Button(12);
-    const pressButton = new five.Button(11);
-  
-    rotaryEncoder({
-      upButton,
-      downButton,
-      pressButton,
-      onUp: () => {
-        enc_val='up';
-        console.log('up');
-      },
-      onDown: () => {
-        enc_val='down';
-        console.log('down');
-      },
-      onPress: () => {
-        enc_val='press';
-       
-        console.log('press');
-      },
-    });
+  const upButton = new five.Button(13);
+  const downButton = new five.Button(12);
+  const pressButton = new five.Button(11);
+
+  rotaryEncoder({
+    upButton,
+    downButton,
+    pressButton,
+    onUp: () => {
+      enc_val='down';
+      enc_change = true;
+      console.log('down');
+      sendVal(enc_val);
+    },
+    onDown: () => {
+      enc_val='up';
+      console.log('up');
+      enc_change = true;
+      sendVal(enc_val);
+    },
+    onPress: () => {
+      enc_val='press';
+      console.log('press');
+      enc_change = true;
+      sendVal(enc_val);
+    },
   });
 
-
-io.on('connection', (client) => {
-  client.on('subscribeToTimer', (interval) => {
-    console.log('client is subscribing to timer with interval ', interval);
-    setInterval(() => {
-      client.emit('timer', enc_val);
-    }, interval);
-  });
 });
 
-app.listen(process.env.PORT || 8080);
-io.listen(8000);
-console.log('listening on port ' + 8000);
+io.on('connection', (client) => {
+  sendVal = (data) => {
+    client.emit('timer', enc_val);
+  }
+})
+
+/*
+io.on('connection', (client) => {
+client.on('subscribeToTimer', (interval) => {
+  console.log('client is subscribing to timer with interval ', interval);
+  setInterval(() => {
+    client.emit('timer', enc_val);
+    client.emit('change', enc_change);
+  }, interval);
+});
+});
+*/
